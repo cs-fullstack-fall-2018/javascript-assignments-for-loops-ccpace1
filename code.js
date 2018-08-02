6a597aa4288b93b7787572eccd284ac55cc3f69f
Kevin-CodeCrew <kevin@code-crew.org>
2018-08-02 01:32:47 -0500

    Graded 08-02-2018 01:32:33

--- code.js
+++ code.js
@@ -6,10 +6,21 @@ for (counter=0; counter<= myNumberList.length; counter++)
     if (number<myNumberList[counter])
     {
         number=myNumberList[counter]
-        console.log(number)
+//        console.log(number) // KEY: No need to log this number here
     }
-    else
-    {
-        console.log(number)
-    }
+//    else
+//    {
+//        console.log(number) // KEY: No need to log this number here
+//    }
+    
 }
+
+// KEY: Log largest number at end
+console.log(number)
+
+/*
+	KEY: Really close. Well done!
+
+	Needed some comments :-/
+	Just log largest number at end, but your approach was correct
+*/
